e7cf1b0d96e51844b7ad0fb76c86bac715dbc3d8
hieuha111k@gmail.com <hahieu111k@gmail.com>
2024-12-05 12:49:06 +0700

    Add initial server configuration and settings files

---
+++ Server/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Server.Models;
+
+namespace Server.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Coordinate> Coordinates { get; set; } = null!;
+        public DbSet<EnvironmentalDataEntry> EnvironmentalDataEntries { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Coordinate>().ToTable("Coordinates");
+            modelBuilder.Entity<EnvironmentalDataEntry>().ToTable("EnvironmentalData");
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
